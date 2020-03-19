6db76ff0fb42f31ad4c1f65eb1950b629ab7913f
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-19 12:05:36 +0530

    Add files via upload

---
+++ StarTrek/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+<title>Connect Ms Access Database in Asp.net using C#, VB.NET</title>
+</head>
+<body>
+<form id="form1" runat="server">
+<div>
+<table align="center">
+<tr>
+<td>UserName:</td>
+<td><asp:TextBox ID="txtUsername" runat="server"/></td>
+</tr>
+<tr>
+<td>Education:</td>
+<td><asp:TextBox ID="txtEducation" runat="server"/></td>
+</tr>
+<tr>
+<td>Location:</td>
+<td><asp:TextBox ID="txtLocation" runat="server"/></td>
+</tr>
+<tr>
+<td></td>
+<td><asp:Button ID="btnSubmit" Text="Submit" runat="server" onclick="btnSubmit_Click"/></td>
+</tr>
+<tr>
+<td><b>UserDetails:</b></td>
+<td>
+<asp:GridView ID="gvDetails" runat="server"></asp:GridView>
+</td>
+</tr>
+</table>
+</div>
+</form>
+</body>
+</html>
---
+++ StarTrek/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Data.OleDb;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    OleDbConnection con;
+    OleDbCommand cmd;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        using (con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\BHARGAVA K\Documents\Visual Studio 2008\WebSites\StarTrek\StarTrek.mdb"))
+        {
+            string Query = "INSERT into userdetails(UserName,Education,Location) VALUES(@UserName, @Education, @Location)";
+            cmd = new OleDbCommand(Query,con);
+            cmd.Parameters.AddWithValue("@UserName", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@Education", txtEducation.Text);
+            cmd.Parameters.AddWithValue("@Location", txtLocation.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            txtUsername.Text = "";
+            txtEducation.Text = "";
+            txtLocation.Text = "";
+        }
+        BindUserDetails();
+    }
+    protected void BindUserDetails()
+    {
+        DataSet ds = new DataSet();
+        string strquery = "SELECT * FROM userdetails";
+        using (con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\BHARGAVA K\Documents\Visual Studio 2008\WebSites\StarTrek\StarTrek.mdb"))
+        {
+            using (cmd = new OleDbCommand(strquery, con))
+            {
+                OleDbDataAdapter Da = new OleDbDataAdapter(cmd);
+                Da.Fill(ds);
+            }
+        }
+        gvDetails.DataSource = ds;
+        gvDetails.DataBind();
+    }
+}
